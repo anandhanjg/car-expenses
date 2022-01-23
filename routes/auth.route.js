c2f77930ab41f8967ae0a9e48b867bba3c442f5d
anandhanjg <jganandhanddevlabs@gmail.com>
2022-01-23 21:45:52 +0530

    test

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,7 +4,7 @@ const { auth } = require("../middlewares/auth.middleware");
 module.exports.authRouter=(express)=>{
     let authRouter=express.Router();
     authRouter.get('/',(req,res)=>{
-        res.json({message:"Auth Routes"});
+        res.json({message:"Welcome Auth Routes"});
     });
     authRouter.post('/register',authController.register);
     authRouter.post('/signin',authController.signin);
